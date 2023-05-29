efbd3b41cee28995ed840bb87e2f78641593f049
DaradSN <darasnunes@gmail.com>
2023-05-28 22:16:24 -0300

    Importei a lista e abri printando na pagina html.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
+//front
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import cards from './data/tarot/tarot.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(cards["cards"]);
+const data = cards["cards"];  //data recebe a lista de objetos
+const ulElement = document.querySelector("ul");
+
+for (const object of data) {  // para cada  obejto da lista data
+  const liElement = document.createElement("li")
+  if (object.type === "major") { // se o o tipo da carta for major 
+    liElement.textContent = object.name;
+    liElement.className = "listCards";
+    ulElement.appendChild(liElement);
+    console.log(object)
+  }
+}
+
+for (const object of data) {  // para cada  obejto da lista data
+  const liElement = document.createElement("li")
+  if (object.type === "major") { // se o o tipo da carta for major 
+    liElement.textContent = object.name;
+    liElement.className = "listCards";
+    ulElement.appendChild(liElement);
+    console.log(object)
+  }
+}
+
+
