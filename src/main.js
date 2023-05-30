c95fcdb8c858eaba469e3767d5b7bbcd8f3fb232
DaradSN <darasnunes@gmail.com>
2023-05-30 09:13:40 -0300

    some changes - gabe note commit

--- src/main.js
+++ src/main.js
@@ -1,31 +1,42 @@
 //front
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+//import { example } from './data.js';
 import cards from './data/tarot/tarot.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+
 
 console.log(cards["cards"]);
+const major = document.querySelector("#cards-major");
+const minors = document.querySelector("#cards-minors");
 const data = cards["cards"];  //data recebe a lista de objetos
-const ulElement = document.querySelector("ul");
+const ulElement = document.querySelector("#listCards");
+
 
 for (const object of data) {  // para cada  obejto da lista data
   const liElement = document.createElement("li")
-  if (object.type === "major") { // se o o tipo da carta for major 
+  if (object.name) { // se o o tipo da carta for major 
     liElement.textContent = object.name;
     liElement.className = "listCards";
     ulElement.appendChild(liElement);
-    console.log(object)
   }
 }
 
 for (const object of data) {  // para cada  obejto da lista data
-  const liElement = document.createElement("li")
-  if (object.type === "major") { // se o o tipo da carta for major 
-    liElement.textContent = object.name;
-    liElement.className = "listCards";
-    ulElement.appendChild(liElement);
-    console.log(object)
+  const liElement = document.createElement("li");
+  liElement.textContent = object.name;
+
+  if (object.type === "minor") { // se o o tipo da carta for major 
+    liElement.className = "listMinors";
+    minors.appendChild(liElement); //criar tag li n=dentro do ul
+  }
+  else if (object.type === "major") { // se o o tipo da carta for major 
+    liElement.className = "listMajor";
+    major.appendChild(liElement);
   }
 }
+
+
+
+
+
+
 
 
