b263e7a61839f97ceb44f94f534b7e2f04515baf
Anara-carolina <anaramartins21@gmail.com>
2023-06-09 11:56:21 -0300

    ajustes filtro

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import cardData from './tarot.js';
 
+// Selecionar o container onde os cards serão exibidos
 const container = document.querySelector('#cardContainer');
+
+// Selecionar os links de filtro
 const linkBigger = document.querySelector('.bigger');
 const linkMinors = document.querySelector('.minors');
 const linkCardall = document.querySelector('.cardall');
@@ -8,15 +11,17 @@ const linkCopas = document.querySelector('.copas');
 const linkOuros = document.querySelector('.ouros');
 const linkPaus = document.querySelector('.paus');
 const linkEspadas = document.querySelector('.espadas');
-let currentFilter = ''; // Variável para armazenar o tipo de filtro selecionado
 
-// Criar os cards e adicionar ao container
+// Variável para armazenar o tipo de filtro selecionado
+let currentFilter = '';
+
+// Criar os cards e adicioná-los ao container
 for (let i = 0; i < cardData.length; i++) {
   const card = createCardElement(cardData[i]);
-
   container.appendChild(card);
 }
 
+// Função para virar o card ao ser clicado
 function flipCard(card) {
   card.classList.toggle('flipped');
 
@@ -36,9 +41,10 @@ function flipCard(card) {
   }
 }
 
+// Função para criar um elemento de card com base nos dados fornecidos
 function createCardElement(data) {
   const card = document.createElement('div');
-  card.classList.add('card', 'card-item', 'clickable-card', data.type, data.suit); // Adicione a classe de tipo ao card
+  card.classList.add('card', 'card-item', 'clickable-card', data.type, data.suit);
 
   const frontElement = document.createElement('div');
   frontElement.classList.add('card-front');
@@ -72,9 +78,10 @@ function createCardElement(data) {
   return card;
 }
 
+// Função para exibir os cards com base nos filtros selecionados
 function displayCards(filters) {
-  const cards = container.querySelectorAll('.card');
-  cards.forEach((card) => {
+  const cardElements = container.querySelectorAll('.card');
+  cardElements.forEach((card) => {
     let shouldDisplay = false;
     filters.forEach((filter) => {
       if (filter === '' || filter === 'all' || card.classList.contains(filter)) {
@@ -90,78 +97,84 @@ function displayCards(filters) {
   });
 }
 
+// Função para ativar o link de filtro atual e desativar os demais
 function activateFilterLink(link) {
-  // Remove a classe ativa de todos os links
   const filterLinks = document.querySelectorAll('.filtro01 a');
   filterLinks.forEach((filterLink) => {
     filterLink.classList.remove('active');
   });
 
-  // Adiciona a classe ativa ao link atual
   link.classList.add('active');
 }
-
+// Event listener para o link "Arcanos Maiores"
 linkBigger.addEventListener('click', function () {
-  if (currentFilter !== 'maior') { // Verifica se o filtro já está ativo
+  if (currentFilter !== 'maior') { 
     currentFilter = 'maior';
     activateFilterLink(linkBigger);
     displayCards('maior'); // Exibe apenas os arcanos maiores
   }
 });
 
+// Event listener para o link "Arcanos Menores"
 linkMinors.addEventListener('click', function () {
-  if (currentFilter !== 'menor') { // Verifica se o filtro já está ativo
+  if (currentFilter !== 'menor') { 
     currentFilter = 'menor';
     activateFilterLink(linkMinors);
     displayCards('menor'); // Exibe apenas os arcanos menores
   }
 });
 
+// Event listener para o link "Todas as Cartas"
 linkCardall.addEventListener('click', function () {
-  if (currentFilter !== 'all') { // Verifica se o filtro já está ativo
+  if (currentFilter !== 'all') {
     currentFilter = 'all';
     activateFilterLink(linkCardall);
-    displayCards([]); // Exibe todas as cartas
+    displayCards('all'); // Exibe todas as cartas, passando uma lista vazia
   }
 });
 
+// Event listener para o link "Copas"
 linkCopas.addEventListener('click', function () {
-  if (currentFilter !== 'copas') { // Verifica se o filtro já está ativo
+  if (currentFilter !== 'copas') { 
     currentFilter = 'copas';
     activateFilterLink(linkCopas);
     displayCards('copas'); // Exibe apenas as cartas de copas
   }
 });
 
+// Event listener para o link "Ouros"
 linkOuros.addEventListener('click', function () {
-  if (currentFilter !== 'ouros') { // Verifica se o filtro já está ativo
+  if (currentFilter !== 'ouros') {
     currentFilter = 'ouros';
     activateFilterLink(linkOuros);
     displayCards('ouros'); // Exibe apenas as cartas de ouros
   }
 });
 
+// Event listener para o link "Paus"
 linkPaus.addEventListener('click', function () {
-  if (currentFilter !== 'paus') { // Verifica se o filtro já está ativo
+  if (currentFilter !== 'paus') { 
     currentFilter = 'paus';
     activateFilterLink(linkPaus);
     displayCards('paus'); // Exibe apenas as cartas de paus
   }
 });
 
+// Event listener para o link "Espadas"
 linkEspadas.addEventListener('click', function () {
-  if (currentFilter !== 'espadas') { // Verifica se o filtro já está ativo
+  if (currentFilter !== 'espadas') { 
     currentFilter = 'espadas';
     activateFilterLink(linkEspadas);
     displayCards('espadas'); // Exibe apenas as cartas de espadas
   }
 });
 
+// Obter os parâmetros de filtro da URL, se houver
 const urlParams = new URLSearchParams(window.location.search);
 const filterType = urlParams.get('type');
 const filterSuit = urlParams.get('suit');
 
-// Cria uma lista de filtros
+// Cria uma lista de filtros com base nos parâmetros da URL
 const filters = [];
 if (filterType) {
   filters.push(filterType);
@@ -172,3 +185,4 @@ if (filterSuit) {
 
 // Exibe os cards com base nos filtros
 displayCards(filters);
+
