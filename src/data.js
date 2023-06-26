3f70cbba08d36993cc272f2f9fa9d3a7a49edfef
DaradSN <darasnunes@gmail.com>
2023-06-26 00:18:49 -0300

    ordened by alphabet complete

--- src/data.js
+++ src/data.js
@@ -1,113 +1,112 @@
 /*const cardTarot =[]
 export default cardTarot;*/
 
-import cardData from "./tarot";
+//import cardData from "./tarot";
 
 const container = document.querySelector("#cardContainer");
 function createCardElement(data) {
-    const card = document.createElement("div");
-    card.classList.add(
-      "card",
-      "card-item",
-      "clickable-card",
-      data.type,
-      data.suit
-    );
+  const card = document.createElement("div");
+  card.classList.add(
+    "card",
+    "card-item",
+    "clickable-card",
+    data.type,
+    data.suit,
+    data.name.replace(/\s/g, "")
+  );
 
-    const frontElement = document.createElement("div");
-    frontElement.classList.add("card-front");
-  
-    const imageElement = document.createElement("img");
-    imageElement.src = data.img;
-    imageElement.classList.add("card-image");
-  
-    frontElement.appendChild(imageElement);
-    card.appendChild(frontElement);
-  
-    const backElement = document.createElement("div");
-    backElement.classList.add("card-back");
-  
-    const nameElement = document.createElement("h2");
-    nameElement.textContent = data.name;
-    nameElement.classList.add("hidden");
-  
-    const meaningElement = document.createElement("p");
-    meaningElement.textContent = data.meaning_up;
-    meaningElement.classList.add("hidden");
-  
-    backElement.appendChild(nameElement);
-    backElement.appendChild(meaningElement);
-    card.appendChild(backElement);
-  
-    // flip
-    card.addEventListener("click", function () {
-      flipCard(card);
-    });
-  
-    return card;
-    // Função para virar o card ao ser clicado
-    function flipCard(card) {
-        card.classList.toggle("flipped");
-    
-        const imageElement = card.querySelector(".card-image");
-        const textElements = card.querySelectorAll(".card-back h2, .card-back p");
-    
-        if (card.classList.contains("flipped")) {
-        imageElement.style.display = "none";
-        textElements.forEach((element) => {
-            element.classList.remove("hidden");
-        });
-        } else {
-        imageElement.style.display = "block";
-        textElements.forEach((element) => {
-            element.classList.add("hidden");
-        });
-        }
+  const frontElement = document.createElement("div");
+  frontElement.classList.add("card-front");
+
+  const imageElement = document.createElement("img");
+  imageElement.src = data.img;
+  imageElement.classList.add("card-image");
+
+  frontElement.appendChild(imageElement);
+  card.appendChild(frontElement);
+
+  const backElement = document.createElement("div");
+  backElement.classList.add("card-back");
+
+  const nameElement = document.createElement("h2");
+  nameElement.textContent = data.name;
+  nameElement.classList.add("hidden");
+
+  const meaningElement = document.createElement("p");
+  meaningElement.textContent = data.meaning_up;
+  meaningElement.classList.add("hidden");
+
+  backElement.appendChild(nameElement);
+  backElement.appendChild(meaningElement);
+  card.appendChild(backElement);
+
+  // flip
+  card.addEventListener("click", function () {
+    flipCard(card);
+  });
+
+  return card;
+  // Função para virar o card ao ser clicado
+  function flipCard(card) {
+    card.classList.toggle("flipped");
+
+    const imageElement = card.querySelector(".card-image");
+    const textElements = card.querySelectorAll(".card-back h2, .card-back p");
+
+    if (card.classList.contains("flipped")) {
+      imageElement.style.display = "none";
+      textElements.forEach((element) => {
+        element.classList.remove("hidden");
+      });
+    } else {
+      imageElement.style.display = "block";
+      textElements.forEach((element) => {
+        element.classList.add("hidden");
+      });
     }
+  }
 }
-  
+
 // Função para criar um elemento de card com base nos dados fornecidos
 function displayCards(filters) {
-    const cardElements = container.querySelectorAll(".card");
-    cardElements.forEach((card) => {
-      let shouldDisplay = false;
-      filters.forEach((filter) => {
-        if (
-          filter === "" ||
-          filter === "all" ||
-          card.classList.contains(filter)
-        ) {
-          shouldDisplay = true;
-        }
-      });
-  
-      if (shouldDisplay) {
-        card.classList.remove("hidden");
-      } else {
-        card.classList.add("hidden");
+  const cardElements = container.querySelectorAll(".card");
+  cardElements.forEach((card) => {
+    let shouldDisplay = false;
+    filters.forEach((filter) => {
+      if (
+        filter === "" ||
+        filter === "all" ||
+        card.classList.contains(filter)
+      ) {
+        shouldDisplay = true;
       }
+    });
+
+    if (shouldDisplay) {
+      card.classList.remove("hidden");
+    } else {
+      card.classList.add("hidden");
+    }
   });
 }
 // Função para ativar o link de filtro atual e desativar os demais
 function activateFilterLink(link) {
-    const filterLinks = document.querySelectorAll(".filtro01 a");
-    filterLinks.forEach((filterLink) => {
-      filterLink.classList.remove("active");
-    });
-  
-    link.classList.add("active");
+  const filterLinks = document.querySelectorAll(".filtro01 a");
+  filterLinks.forEach((filterLink) => {
+    filterLink.classList.remove("active");
+  });
 
-  }
-
+  link.classList.add("active");
+}
 
 // Função para ordenar as cartas por nome
 function sortCardsByName(cards, sortOrder) {
   cards.sort((a, b) => {
     const nameA = a.name.toUpperCase();
     const nameB = b.name.toUpperCase();
-    if (sortOrder === "name-asc") {
+    if (sortOrder == "name-asc") {
       return nameA.localeCompare(nameB);
-    } else if (sortOrder === "name-desc") {
+    } else if (sortOrder == "name-desc") {
       return nameB.localeCompare(nameA);
     } else {
       return 0;
@@ -115,6 +114,4 @@ function sortCardsByName(cards, sortOrder) {
   });
 }
 
-
-export { createCardElement, displayCards,activateFilterLink, sortCardsByName};
-
+export { createCardElement, displayCards, activateFilterLink, sortCardsByName };
--- src/main.js
+++ src/main.js
@@ -16,11 +16,13 @@ const linkOuros = document.querySelector(".ouros");
 const linkPaus = document.querySelector(".paus");
 const linkEspadas = document.querySelector(".espadas");
 
+const linkOrda = document.querySelector(".orda");
+const linkOrdz = document.querySelector(".ordz");
+
 const urlParams = new URLSearchParams(window.location.search);
 const filterType = urlParams.get("type");
 const filterSuit = urlParams.get("suit");
 const sortParam = urlParams.get("sort");
-let currentFilter = "";
 
 // Limpa o container antes de adicionar os cards
 container.innerHTML = "";
@@ -29,8 +31,6 @@ for (let i = 0; i < cardData.length; i++) {
   const card = createCardElement(cardData[i]);
   container.appendChild(card);
 }
-
-console.log(currentFilter);
 
 // Exibe os cards com base nos filtros
 const filters = [];
@@ -77,105 +77,61 @@ if (filters.length === 1) {
   titleElement.textContent = "";
 }
 
-// Event listener para o link "Arcanos Maiores"
-linkBigger.addEventListener("click", function () {
-  if (currentFilter !== "maior") {
-    currentFilter = "maior";
-    activateFilterLink(linkBigger);
-    displayCards(["maior"]); // Exibe apenas os arcanos maiores
-  }
-});
-
-// Event listener para o link "Arcanos Menores"
-linkMinors.addEventListener("click", function () {
-  if (currentFilter !== "menor") {
-    currentFilter = "menor";
-    activateFilterLink(linkMinors);
-    displayCards(["menor"]); // Exibe apenas os arcanos menores
-  }
-});
-
-// Event listener para o link "Todas as Cartas"
-linkCardall.addEventListener("click", function () {
-  if (currentFilter !== "all") {
-    currentFilter = "all";
-    activateFilterLink(linkCardall);
-    displayCards(["all"]); // Exibe todas as cartas, passando uma lista vazia
-  }
-});
-
-// Event listener para o link "Copas"
-linkCopas.addEventListener("click", function () {
-  if (currentFilter !== "copas") {
-    currentFilter = "copas";
-    activateFilterLink(linkCopas);
-    displayCards(["copas"]); // Exibe apenas as cartas de copas
-  }
-});
-
-// Event listener para o link "Ouros"
-linkOuros.addEventListener("click", function () {
-  if (currentFilter !== "ouros") {
-    currentFilter = "ouros";
-    activateFilterLink(linkOuros);
-    displayCards(["ouros"]); // Exibe apenas as cartas de ouros
-  }
-});
-
-// Event listener para o link "Paus"
-linkPaus.addEventListener("click", function () {
-  if (currentFilter !== "paus") {
-    currentFilter = "paus";
-    activateFilterLink(linkPaus);
-    displayCards(["paus"]); // Exibe apenas as cartas de paus
-  }
-});
-
-// Event listener para o link "Espadas"
-linkEspadas.addEventListener("click", function () {
-  if (currentFilter !== "espadas") {
-    currentFilter = "espadas";
-    activateFilterLink(linkEspadas);
-    displayCards(["espadas"]); // Exibe apenas as cartas de espadas
-  }
-});
-
-const filterLinks = document.querySelectorAll(".filtro01 a");
-
-filterLinks.forEach((filterLink) => {
-  filterLink.addEventListener("click", function () {
-    const filter = filterLink.getAttribute("data-filter");
-
-    if (currentFilter !== filter) {
-      currentFilter = filter;
-      activateFilterLink(filterLink);
-      displayCards([filter]);
-    }
+const showInDisplay = (link) => {
+  link.addEventListener("click", () => {
+    activateFilterLink(link);
+    displayCards(filters); // Exibe apenas os arcanos maiores
   });
-});
+};
 
-function deepCopy(obj) {
-  if (typeof obj !== "object" || obj === null) {
-    // Se não for um objeto, retornar o valor original
-    return obj;
-  }
-  // Verificar se é um array ou objeto
-  const copy = Array.isArray(obj) ? [] : {};
-  // Fazer uma cópia de cada propriedade ou elemento aninhado
-  for (const key in obj) {
-    if (Object.hasOwnProperty.call(obj, key)) {
-      copy[key] = deepCopy(obj[key]);
-    }
-  }
+showInDisplay(linkBigger);
+showInDisplay(linkMinors);
+showInDisplay(linkCardall);
+showInDisplay(linkCopas);
+showInDisplay(linkOuros);
+showInDisplay(linkPaus);
+showInDisplay(linkEspadas);
 
-  return copy;
-}
-
-copyList = deepCopy(cardData);
-console.log(cardData);
-console.log(copyList);
-
+//sortCardsByName(card,sortOrder)
+// Aplica a ordenação se o parâmetro de ordenação estiver presente
 displayCards(filters);
 
-// Aplica a ordenação se o parâmetro de ordenação estiver presente
-sortCardsByName(copyList, sortParam);
+const allCards = document.querySelectorAll(".card");
+
+if (sortParam !== "none") {
+  let cssClass = "";
+  let listClass = [];
+  let ordenedElements = [];
+
+  allCards.forEach((card) => {
+    cssClass = card.classList;
+    listClass.push(cssClass[5]);
+  });
+
+  if (sortParam === "name-asc") {
+
+    listClass.sort((a, b) => a.localeCompare(b, undefined, { ignorePunctuation: true }));
+
+    ordenedElements = Array.from(allCards).sort((a, b) => {
+      const classeA = a.classList[a.classList.length - 2];
+      const classeB = b.classList[b.classList.length - 2];
+      return listClass.indexOf(classeA) - listClass.indexOf(classeB);
+    });
+
+  } else if (sortParam === "name-desc") {
+
+    listClass.sort((a, b) => b.localeCompare(a, undefined, { ignorePunctuation: true }));
+
+    ordenedElements = Array.from(allCards).sort((a, b) => {
+      const classeA = a.classList[a.classList.length - 2];
+      const classeB = b.classList[b.classList.length - 2];
+      return listClass.indexOf(classeA) - listClass.indexOf(classeB);
+    });
+  }
+
+  ordenedElements.forEach(element => {
+    element.classList.remove("hidden");
+    container.appendChild(element);
+  });
+
+}
